e8deae07e43bf6c06b787bc444abf26ea4788eda
eugodr <egordorichev@gmail.com>
2022-04-02 14:06:54 +0300

    Basic world grid

--- src/entity/Camera.js
+++ src/entity/Camera.js
@@ -3,7 +3,11 @@ import Entity from './Entity'
 export default class Camera extends Entity {
 	constructor(target) {
 		super()
+
 		this.target = target
+		this.width = 0
+		this.height = 0
+		this.scale = 3
 	}
 
 	update(p5, dt) {
@@ -14,15 +18,28 @@ export default class Camera extends Entity {
 	}
 
 	apply(p5) {
-		let scale = 3
-
-		p5.scale(scale)
+		p5.scale(this.scale)
 		p5.translate(
-			-this.x + p5.windowWidth / 2 / scale,
-			-this.y / 2 + p5.windowHeight / 2 / scale)
+			-this.x + p5.windowWidth / 2 / this.scale,
+			-this.y + p5.windowHeight / 2 / this.scale)
 	}
 
 	renderGrid(p5) {
-		
+		p5.stroke(100, 100, 100, 50)
+		p5.strokeWeight(1)
+
+		const doubleScale = 2 * this.scale
+		const width = p5.windowWidth
+		const height = p5.windowHeight
+		const cx = this.x
+		const cy = this.y
+
+		for (var x = (cx - width / doubleScale) - cx % 32; x <= cx + 8 + width / doubleScale; x += 32) {
+			p5.line(x, cy - height / doubleScale, x, cy + 8 + height / doubleScale)
+		}
+
+		for (var y = (cy - height / doubleScale) - cy % 32; y <= cy + 8 + height / doubleScale; y += 32) {
+			p5.line(cx - width / doubleScale, y, cx + 8 + width / doubleScale, y)
+		}
 	}
 }
