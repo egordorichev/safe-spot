149091349b89992c29ea3727a482b2723cc55eb0
eugodr <egordorichev@gmail.com>
2022-04-02 12:56:54 +0300

    Basic p5 canvas setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Sketch from 'react-p5'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+	setup(p5, ref) {
+		this.canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight)
+		this.canvas.parent(ref)
+	}
+
+	draw(p5) {
+		p5.background(0)
+		p5.ellipse(70, 70, 70, 70)
+	}
+
+	windowResized(p5) {
+		p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Sketch 
+					setup={this.setup.bind(this)} 
+					draw={this.draw.bind(this)}
+					windowResized={this.windowResized.bind(this)}
+				/>
+			</div>
+		)
+	}
 }
-
-export default App;
