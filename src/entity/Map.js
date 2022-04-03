d6e7e44f27d50036657b3cfdfa66a4e1f09bc67f
eugodr <egordorichev@gmail.com>
2022-04-03 12:21:48 +0300

    The darkness consumes the map

--- src/entity/Map.js
+++ src/entity/Map.js
@@ -1,3 +1,4 @@
+import { waitForElementToBeRemoved } from '@testing-library/react'
 import Entity from './Entity'
 
 export default class Map extends Entity {
@@ -8,6 +9,7 @@ export default class Map extends Entity {
 		this.minY = 0
 		this.maxX = 0
 		this.maxY = 0
+		this.time = 0
 	}
 
 	init(p5) {
@@ -39,6 +41,47 @@ export default class Map extends Entity {
 		}
 
 		return array[x] ?? 0
+	}
+
+	update(p5, dt) {
+		this.time += dt * 0.0001
+
+		if (this.time >= 1) {
+			this.time = 0
+			let toRemove = []
+
+			for (let y = this.minY; y <= this.maxY; y++) {
+				let row = this.tiles[y]
+	
+				if (!row) {
+					continue
+				}
+				
+				for (let x = this.minX; x <= this.maxX; x++) {
+					let tile = row[x]
+	
+					if (tile && tile > 0) {
+						let neighbours = 0
+
+						for (let ox = -1; ox <= 1; ox++) {
+							for (let oy = -1; oy <= 1; oy++) {
+								if ((ox != 0 || oy != 0) && this.getTile(x + ox, y + oy) != 0) {
+									neighbours++
+								}
+							}
+						}
+
+						if (neighbours < 5 && Math.random() < 0.05) {
+							toRemove.push([ x, y ])
+						}
+					}
+				}
+			}
+
+			toRemove.forEach(coords => {
+				this.setTile(coords[0], coords[1], 0)
+			})
+		}
 	}
 
 	render(p5, canvas) {
